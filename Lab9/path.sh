a18d4d265fd768c269764054b86e9b2b9b7a2d77
Gabrilhrubio <gabrielrubio200531@gmail.com>
2023-10-19 01:12:38 +0000

    Lab9

---
+++ Lab9/path.sh
@@ -0,0 +1,14 @@
+   #!/bin/bash
+   
+   ###############################
+   #File name: Burger_King.sh
+   #Author: Gabriel Rubio
+   #10/18/23
+   # Description: The Burger King song
+   ###############################
+  
+ echo -n "Enter the Directory you wish to view" 
+read directory 
+echo "contents of directory $directory"
+ls $directory 
+
